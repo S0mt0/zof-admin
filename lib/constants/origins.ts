d13282279d5d0fd12341dd57396cbff2072ec3c8
S0mt0 <sewkito@gmail.com>
2026-05-30 18:51:53 +0100

    debug google auth successful

--- lib/constants/origins.ts
+++ lib/constants/origins.ts
@@ -1,17 +1,24 @@
 type NodeEnv = "development" | "production" | "test" | "preview" | "staging";
 
 const NODE_ENV = process.env.NODE_ENV as NodeEnv;
+const VERCEL_ENV = process.env.VERCEL_ENV as NodeEnv;
 
 export const APP_URL =
   NODE_ENV === "development"
     ? "http://localhost:3000"
-    : NODE_ENV === "preview" || NODE_ENV === "staging"
+    : NODE_ENV === "preview" ||
+      NODE_ENV === "staging" ||
+      VERCEL_ENV === "preview" ||
+      VERCEL_ENV === "staging"
     ? "https://demo.admin.zitaonyekafoundation.org"
     : "https://admin.zitaonyekafoundation.org";
 
 export const FRONTEND_BASE_URL =
   NODE_ENV === "development"
     ? "http://localhost:3001"
-    : NODE_ENV === "preview" || NODE_ENV === "staging"
+    : NODE_ENV === "preview" ||
+      NODE_ENV === "staging" ||
+      VERCEL_ENV === "preview" ||
+      VERCEL_ENV === "staging"
     ? "https://demo.admin.zitaonyekafoundation.org"
     : "https://www.zitaonyekafoundation.org";
--- lib/constants/routes.ts
+++ lib/constants/routes.ts
@@ -14,7 +14,6 @@ export const publicApiRoutes = [
   "/api/teams",
   "/api/testimonials",
   "/api/volunteers",
-  "/api/debug-auth",
 ];
 
 export const authRoutes = [
